cbf19fbd2ce0a63c1bcf46e38f7a1d41f783d001
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-10 20:13:13 +0600

    link extractor added

--- tutorial/tutorial/spiders/flipbooks.py
+++ tutorial/tutorial/spiders/flipbooks.py
@@ -1,10 +1,17 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class FlipbooksSpider(scrapy.Spider):
+class FlipbooksSpider(CrawlSpider):
     name = 'flipbooks'
     allowed_domains = ['flip.kz']
-    start_urls = ['http://flip.kz/']
+    start_urls = ['https://www.flip.kz/']
 
-    def parse(self, response):
-        pass
+    rules = (Rule(LinkExtractor(allow=r'flip.kz'),
+                  callback='parse_item', follow=True),)
+
+    def parse_item(self, response):
+        url = response.request.url
+        if 'flip.kz/catalog?prod' in url:
+            print('Found item link: ', url)
