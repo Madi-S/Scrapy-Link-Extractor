bcb25de827eedff3e556f84417451ad2242900d3
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-10 20:43:23 +0600

    rules and parse strategy modified

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -4,3 +4,4 @@ import scrapy
 class TutorialItem(scrapy.Item):
     title = scrapy.Field()
     price = scrapy.Field()
+    description = scrapy.Field()
--- tutorial/tutorial/spiders/flipbooks.py
+++ tutorial/tutorial/spiders/flipbooks.py
@@ -1,17 +1,24 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from ..items import TutorialItem
 
 
 class FlipbooksSpider(CrawlSpider):
     name = 'flipbooks'
     allowed_domains = ['flip.kz']
     start_urls = ['https://www.flip.kz/']
-
-    rules = (Rule(LinkExtractor(allow=r'flip.kz'),
+    rules = (Rule(LinkExtractor(allow=''),
                   callback='parse_item', follow=True),)
 
     def parse_item(self, response):
-        url = response.request.url
-        if 'flip.kz/catalog?prod' in url:
-            print('Found item link: ', url)
+        if 'catalog' in str(response.url):
+
+            item = TutorialItem()
+            item['title'] = response.xpath('//h1/text()').get()
+            item['price'] = response.xpath(
+                '//span[@class="text_att"]//b/text()').get()
+            item['description'] = response.xpath(
+                '//span[@itemprop="description"]/text()').get()
+
+            yield item
